cc39f8a78bda75154e50b8bb45d6601cc438628e
Venon <manesvenom@gmail.com>
2022-05-27 11:43:46 +0800

    Test Fire only

--- web.js
+++ web.js
@@ -11,7 +11,8 @@ app.get('/', function (req, res) {
 app.post('/', function (req, res) {
   console.log(req.body);
   const moves = ['F', 'T', 'L', 'R'];
-  res.send(moves[Math.floor(Math.random() * moves.length)]);
+  res.send('T');
+ // res.send(moves[Math.floor(Math.random() * moves.length)]);
 });
 
 app.listen(process.env.PORT || 8080);
