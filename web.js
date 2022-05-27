c83b9b9b3c58e1ac1d46e952adf90d2b5df4871a
Venon <manesvenom@gmail.com>
2022-05-27 13:56:04 +0800

    add findPositon

--- web.js
+++ web.js
@@ -9,10 +9,45 @@ app.get('/', function (req, res) {
 });
 
 app.post('/', function (req, res) {
-  console.log(req.body);
-  const moves = ['F', 'T', 'L', 'R'];
-  res.send('T');
- // res.send(moves[Math.floor(Math.random() * moves.length)]);
+  //console.log(req.body);
+
+  data = req.body;
+  var dim = data.arena.dims; 
+  console.log(dim[0]);
+  console.log(dim[1]);
+  var state = data.arena.state;
+  console.log(data.arena.state);
+
+  let nextPosition = findPosition(data._links.self.href,state,dim);
+  console.log(nextPosition);
+
+  res.send(nextPosition);
+
 });
+
+function findPosition(myLink, state,dim){
+  
+  let keys =  Object.keys(state);
+  let sprite = state[myLink];
+
+   var centerX = Math.floor(dim[0]/2);
+   var centerY = Math.floor(dim[1]/2);
+
+    var spriteX = sprite.x;
+    var spriteY = sprite.y;
+
+    if(spriteX > centerX){
+      return 'F';
+    }else{
+      const moves = [ 'T', 'L', 'R'];
+      return (moves[Math.floor(Math.random() * moves.length)]);
+    }
+
+
+}
+
+
+
+
 
 app.listen(process.env.PORT || 8080);
